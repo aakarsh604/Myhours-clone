9f4612e0d4c98421c99cb2aaa3426793d3cc9515
aakarsh604 <aakarsh604@gmail.com>
2022-07-19 19:14:12 +0530

    added chakra

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
