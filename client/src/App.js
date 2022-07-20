cd2049a89cde5e60cf374339a6f1998c51a0381e
APURVA-DIVAKAR <99614708+APURVA-DIVAKAR@users.noreply.github.com>
2022-07-20 11:06:50 +0530

    Footer Done

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router';
 import Homepage from './pages/Homepage/Homepage';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
      <Routes>
       <Route path="/" element={<Homepage/>}/>
      </Routes>
+     <Footer/>
     </div>
   );
 }
