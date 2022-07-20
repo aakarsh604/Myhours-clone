6dca39eab915448f3e843d9960ae022537bdb641
APURVA-DIVAKAR <99614708+APURVA-DIVAKAR@users.noreply.github.com>
2022-07-20 10:09:18 +0530

    branch

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Route, Routes } from 'react-router';
 import Homepage from './pages/Homepage/Homepage';
 
+
 function App() {
   return (
     <div className="App">
      <Routes>
       <Route path="/" element={<Homepage/>}/>
      </Routes>
+     
     </div>
   );
 }
