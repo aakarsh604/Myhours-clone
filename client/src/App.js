0eb52d1b734f24bee51989ac204c5d48915018f3
aakarsh604 <aakarsh604@gmail.com>
2022-07-21 01:57:10 +0530

    added usecase page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 
-// import './App.css';
+import './App.css';
 import { Route, Routes } from 'react-router';
 import Homepage from './pages/Homepage/Homepage';
 import { Signin } from './pages/Signin/Signin';
 import Footer from './components/Footer/Footer';
 import Header from './components/Hedear/Header';
+import Usecase from './pages/UsecasePage/Usecase';
 
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
      <Header/>
      <Routes>
       <Route path="/" element={<Homepage/>}/>
-      <Route path='/signup' element={<Signin/>}/>
+      <Route path='/signup' element={<Signin/>}/> 
+      <Route path='/usecase' element={<Usecase/>}/> 
      </Routes>
      <Footer/>
     </div>
