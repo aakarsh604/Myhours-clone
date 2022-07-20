9b84bfc0c9c7c8dcdf053610e811fd990b8d0103
APURVA-DIVAKAR <99614708+APURVA-DIVAKAR@users.noreply.github.com>
2022-07-20 23:25:25 +0530

    Header

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,8 +1,18 @@
+import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <Box fontFamily={'sans-serif'} mt={{md:'10rem'}}>
+       {/* 1st part */}
+       <Box w={'80%'} m={'auto'}>
+          <Text size={'3xl'} align="center''">
+            Organize projects, Track time and Report your work
+
+          </Text>
+       </Box>
+       
+    </Box>
   )
 }
 
