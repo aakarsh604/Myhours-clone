47bf2eedac24fff825ec332e45bb756fbfaa3460
APURVA-DIVAKAR <99614708+APURVA-DIVAKAR@users.noreply.github.com>
2022-07-20 12:02:38 +0530

    footer final

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Route, Routes } from 'react-router';
 import Homepage from './pages/Homepage/Homepage';
 import Footer from './components/Footer/Footer';
+import Header from './components/Hedear/Header';
 
 function App() {
   return (
     <div className="App">
+     <Header/>
      <Routes>
       <Route path="/" element={<Homepage/>}/>
      </Routes>
