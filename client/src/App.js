fe40f5562f7582ea24db22bf63e99de7802ae623
Aarif5435 <97459053+Aarif5435@users.noreply.github.com>
2022-07-21 01:39:30 +0530

    backend done for signup and signin

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 
-import './App.css';
+// import './App.css';
 import { Route, Routes } from 'react-router';
 import Homepage from './pages/Homepage/Homepage';
+import { Signin } from './pages/Signin/Signin';
 
 function App() {
   return (
     <div className="App">
      <Routes>
       <Route path="/" element={<Homepage/>}/>
+      <Route path='/signup' element={<Signin/>}/>
      </Routes>
     </div>
   );
