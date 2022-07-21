01498ad6ecc9c9b24dc03f286bf8ef630087acfc
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-07-21 17:04:27 +0530

    support page done

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,19 @@ import { Signin } from './pages/Signin/Signin';
 import Footer from './components/Footer/Footer';
 import Header from './components/Hedear/Header';
 import Usecase from './pages/UsecasePage/Usecase';
+import Support from './pages/Support/Support';
 
 
 
 function App() {
   return (
     <div className="App">
-     <Header/>
+     {/* <Header/> */}
      <Routes>
       <Route path="/" element={<Homepage/>}/>
       <Route path='/signup' element={<Signin/>}/> 
       <Route path='/usecase' element={<Usecase/>}/> 
+      <Route path='/support' element={<Support/>}/> 
      </Routes>
      
      <Footer/>
