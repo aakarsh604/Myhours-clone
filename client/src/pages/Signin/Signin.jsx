4050332c663e1f89df10b609356809196ba82898
aakarsh604 <aakarsh604@gmail.com>
2022-07-23 17:59:28 +0530

    added signin page

--- client/src/pages/Signin/Signin.jsx
+++ client/src/pages/Signin/Signin.jsx
@@ -1,70 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-    Flex,
-    Box,
-    FormControl,
-    FormLabel,
-    Input,
-    Checkbox,
-    Stack,
-    Link,
-    Button,
-    Heading,
-    Text,
-    useColorModeValue,
-  } from '@chakra-ui/react';
-  
-const Signin = () => {
-    return (
-      <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        
-        border="1px solid">
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-            <Text fontSize={'lg'} color={'gray.600'}>
-              to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
-            </Text>
-          </Stack>
-          <Box
-            rounded={'lg'}
-            bg={useColorModeValue('white', 'gray.700')}
-            boxShadow={'lg'}
-            p={8}>
-            <Stack spacing={4}>
-              <FormControl id="email">
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel>Password</FormLabel>
-                <Input type="password" />
-              </FormControl>
-              <Stack spacing={10}>
-                <Stack
-                  direction={{ base: 'column', sm: 'row' }}
-                  align={'start'}
-                  justify={'space-between'}>
-                  <Checkbox>Remember me</Checkbox>
-                  <Link color={'blue.400'}>Forgot password?</Link>
-                </Stack>
-                <Button
-                  bg={'blue.400'}
-                  color={'white'}
-                  _hover={{
-                    bg: 'blue.500',
-                  }}>
-                  Sign in
-                </Button>
-              </Stack>
-            </Stack>
-          </Box>
-        </Stack>
-      </Flex>
-    );
-  }
+  Box,
+  Image,
+  Heading,
+  Input,
+  Button,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
-  export default Signin;
+const Signup = () => {
+  const [form, setForm] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <Box pt="150" mb="50">
+      <Box boxShadow="2xl" h="450" w="32%" m="auto" pt="70" p="35">
+        <Image
+          h="39"
+          m="auto"
+          src="https://allhoursproductb0b1.blob.core.windows.net/static-files/myhours_logo_icon.svg"
+        />
+        <Heading fontSize="26" fontWeight="500" mt="30" textAlign="left">
+          Sign In
+        </Heading>
+
+        <FormControl>
+          <FormLabel mt="25" fontSize="sm">
+            EMAIL
+          </FormLabel>
+          <Input
+            id="b"
+            type="email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+            placeholder="Email"
+          />
+
+          <FormLabel mt="25" fontSize="sm">
+            PASSWORD
+          </FormLabel>
+          <Input
+            id="c"
+            type="password"
+            placeholder="Set password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+
+          <Flex alignItems="end" justify="space-between" mb="5">
+            <Button
+              size="sm"
+              w="70px"
+              ml="0"
+              mt="5"
+              colorScheme="blue"
+              onClick={handleSubmit}
+            >
+              Sign In
+            </Button>
+
+            <Text color="blue">Reset Password?</Text>
+          </Flex>
+
+          <hr/>
+
+          <FormHelperText color="blue" fontSize="md">
+            <Link to="/signup">New to My Hours? Sign up</Link>
+          </FormHelperText>
+        </FormControl>
+      </Box>
+    </Box>
+  );
+};
+
+export default Signup;
